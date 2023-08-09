d98ce7c469c23de3a39042178a9be717486deedf
ShengGut <kazuna@Kazunas-MacBook-Pro.local>
2023-08-08 18:38:33 -0700

    Completed Javascript functionality for basic calculator operations

--- script.js
+++ script.js
@@ -1,28 +1,57 @@
-
-function add(a, b) {
-	return a + b;
+function operate (operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '−':
+            return num1 - num2;
+        case '×':
+            return num1 * num2;
+        case '÷':
+            if (num2 === 0)
+                return 0; 
+            return num1 / num2;
+        default:
+            return "Error: not a valid operator";
+    }
 }
 
-function subtract (a, b){
-    return a - b;
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const inputField = document.querySelector("input");
+    const buttons = document.querySelectorAll(".calc-button");
 
-function multiply (a,b) {
-    return a * b;
-  };
+    let currentInput = "";
+    let currentOperator = "";
+    let prevValue = null;
 
-function divide (a,b) {
-    if (b == 0)
-    return 0;
-    else
-    return a / b;
-  };
-
-function operate(operator, a, b) {
-    return operator(a,b);
-}
-
-console.log(add(2,2));
-console.log(subtract(2,2));
-console.log(multiply(2,2));
-console.log(divide(2,2));
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const buttonValue = button.textContent;
+            
+            if (!isNaN(buttonValue) || buttonValue === ".") {
+                currentInput += buttonValue;
+                inputField.value = currentInput;
+            } else if (buttonValue === "C") {
+                currentInput = "";
+                currentOperator = "";
+                prevValue = null;
+                inputField.value = "";
+            } else if (buttonValue === "=") {
+                if (currentInput !== "") {
+                    if (prevValue !== null && currentOperator !== "") {
+                        const result = operate(currentOperator, prevValue, parseFloat(currentInput));
+                        inputField.value = result;
+                        currentInput = result;
+                        prevValue = null;
+                        currentOperator = "";
+                    }
+                }
+            } else {
+                    if (currentInput !== "") {
+                        prevValue = parseFloat(currentInput);
+                        currentInput = "";
+                        currentOperator = buttonValue;
+                    }
+                }
+        });
+    });
+});
